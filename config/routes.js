a87486e6e2ddd779e23e37aa77b532b8398b1c60
MikeyHall <wangyimiao666@gmail.com>
2021-09-10 09:31:23 +0800

    antd pro

---
+++ config/routes.js
@@ -0,0 +1,73 @@
+export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  // {
+  //   path: '/text',
+  //   name: 'text',
+  //   routes: [
+  //     {
+  //       path: '/text/text2',
+  //       name: 'text',
+  //       icon: 'smile',
+  //       component: './text/text2',
+  //     },
+  //     {
+  //       component: './404',
+  //     },
+  //   ],
+  // },
+  {
+    component: './404',
+  },
+]
---
+++ src/pages/text/text2/index.jsx
@@ -0,0 +1,5 @@
+import './index.less'
+
+export default () => {
+  return <div className="text">text2..............</div>
+}
